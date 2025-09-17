1e5ec0f4ca95260558decaf1ac8ee84f50b13a2c
chandini-ai <chandinigurram1111@gmail.com>
2025-09-17 06:57:51 +0530

    Add files via upload

---
+++ startup/script.js
@@ -0,0 +1,7 @@
+// script.js
+const menuBtn = document.getElementById('menu-btn');
+const navLinks = document.getElementById('nav-links');
+
+menuBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
